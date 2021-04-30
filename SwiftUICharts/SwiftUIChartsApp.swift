c6141d9491f329430b2ee3dbee076df5e715b0a9
Ahmed Mgua <mguaahmed@gmail.com>
2021-04-30 23:09:56 +0300

    Initial Commit

---
+++ SwiftUICharts/SwiftUIChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChartsApp.swift
+//  SwiftUICharts
+//
+//  Created by Ahmed Mgua on 4/30/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
